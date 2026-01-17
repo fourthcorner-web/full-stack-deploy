cab93319e07722cb38feff3966e2c2f6983d88f7
fourthcorner-web <fourth.corner.domain@gmail.com>
2026-01-17 22:40:20 +0300

    Update requirements.txt with new dependenciesdd

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Upgrade core tools to fix the 'setuptools' error
+python -m pip install --upgrade pip setuptools wheel
+
+# Install your dependencies
+pip install -r requirements.txt
+
+# Run Django commands
+python manage.py collectstatic --no-input
+python manage.py migrate
