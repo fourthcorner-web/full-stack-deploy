1b6b7f59d6f651d164cf1c9c9e6da95e2e11f6e4
fourthcorner-web <fourth.corner.domain@gmail.com>
2026-02-08 14:57:06 +0300

    Update middleware.py

--- core/middleware.py
+++ core/middleware.py
@@ -1,8 +1,4 @@
-"""
-Custom middleware for Content Security Policy (CSP) headers
-"""
 from django.conf import settings
-
 
 class CSPMiddleware:
     """
@@ -14,30 +10,31 @@ class CSPMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
         
-    if not settings.DEBUG:
-                # Updated CSP policy to support Google Translate and static assets
-                csp_policy = (
-                    "default-src 'self'; "
-                    "script-src 'self' 'unsafe-inline' 'unsafe-eval' "
-                    "https://code.jquery.com https://cdn.jsdelivr.net "
-                    "https://translate.google.com https://translate.googleapis.com https://www.gstatic.com; "
-                    
-                    "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://www.gstatic.com https://fonts.googleapis.com; "
-                    
-                    "img-src 'self' data: https: https://www.gstatic.com https://www.google.com; "
-                    
-                    "font-src 'self' data: https://fonts.gstatic.com; "
-                    
-                    "connect-src 'self' https://translate.google.com https://translate.googleapis.com https://api.emailjs.com; "
-                    
-                    "frame-src 'self' https://www.google.com https://translate.google.com; "
-                    
-                    "object-src 'none'; "
-                    "base-uri 'self'; "
-                    "form-action 'self'; "
-                    "frame-ancestors 'none'; "
-                    "upgrade-insecure-requests;"
-                )
-                response['Content-Security-Policy'] = csp_policy
+        
+        if not settings.DEBUG:
+            
+            csp_policy = (
+                "default-src 'self'; "
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' "
+                "https://code.jquery.com https://cdn.jsdelivr.net "
+                "https://translate.google.com https://translate.googleapis.com https://www.gstatic.com; "
+                
+                "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://www.gstatic.com https://fonts.googleapis.com; "
+                
+                "img-src 'self' data: https: https://www.gstatic.com https://www.google.com; "
+                
+                "font-src 'self' data: https://fonts.gstatic.com; "
+                
+                "connect-src 'self' https://translate.google.com https://translate.googleapis.com https://api.emailjs.com; "
+                
+                "frame-src 'self' https://www.google.com https://translate.google.com; "
+                
+                "object-src 'none'; "
+                "base-uri 'self'; "
+                "form-action 'self'; "
+                "frame-ancestors 'none'; "
+                "upgrade-insecure-requests;"
+            )
+            response['Content-Security-Policy'] = csp_policy
         
         return response
